d2b43486d2ebc6f7759068c4aed9132d9c97914c
Renata Liu <renata_liu@outlook.com>
2025-03-22 12:30:34 -0700

    adding files to folders

---
+++ PhotoFinish/ViewModels/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  PhotoFinish
+//
+//  Created by Renata Liu on 2025-03-22.
+//
+
+import Foundation
---
+++ PhotoFinish/Views/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  PhotoFinish
+//
+//  Created by Renata Liu on 2025-03-22.
+//
+
+import Foundation
