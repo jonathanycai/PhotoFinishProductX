329b15db30d34435d426c811fac4d41c059432f5
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-03-22 12:18:31 -0700

    Initial Commit

---
+++ PhotoFinish/PhotoFinishApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFinishApp.swift
+//  PhotoFinish
+//
+//  Created by Jonathan Cai on 2025-03-22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFinishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
