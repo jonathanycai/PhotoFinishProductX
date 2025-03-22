a2bfecf94ad078b1c442ab73061fd4ac769a6250
Renata Liu <renata_liu@outlook.com>
2025-03-22 14:35:56 -0700

    basic camera view

---
+++ PhotoFinish/Views/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  PhotoFinish
+//
+//  Created by Renata Liu on 2025-03-22.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
