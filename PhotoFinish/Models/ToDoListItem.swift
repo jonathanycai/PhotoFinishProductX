08df3c51aad847d3519685311459bfabf80040ea
Jonathan Cai <jonathan.y.cai@outlook.com>
2025-03-22 17:25:20 -0700

    Created Firebase implementation for creating new Todo's

---
+++ PhotoFinish/Models/ToDoListItem.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListItem.swift
+//  PhotoFinish
+//
+//  Created by Jonathan Cai on 2025-03-22.
+//
+
+import Foundation
+
+struct ToDoListItem: Codable, Identifiable {
+    let id: String
+    let title: String
+    let dueDate: TimeInterval
+    let createdDate: TimeInterval
+    var isDone: Bool
+    
+    mutating func setDone(_ state: Bool) {
+        isDone = state
+    }
+}
--- PhotoFinish/ViewModels/NewItemViewViewModel.swift
+++ PhotoFinish/ViewModels/NewItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Jonathan Cai on 2025-03-22.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 
@@ -16,7 +17,28 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else { return }
         
+        // Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else { return }
+        
+        // Create Model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false
+            )
+        
+        // Save Model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+            
     }
     
     var canSave: Bool {
